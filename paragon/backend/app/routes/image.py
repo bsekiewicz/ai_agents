81dc6ce24c41fb76e9b667c38310eec9819b86b6
Bartosz Sękiewicz <bartosz.pawel.sekiewicz@gmail.com>
2025-03-13 19:36:40 +0100

    1.0.2 > 1.0.3

--- paragon/backend/app/routes/image.py
+++ paragon/backend/app/routes/image.py
@@ -6,7 +6,7 @@ router = APIRouter()
 @router.post("/ocr-image")
 async def upload_and_ocr_image(
     file: UploadFile = File(...),
-    prompt_version: str = Form("1_0_2"),
+    prompt_version: str = Form("1_0_3"),
 ):
     result = await ocr_image(file, prompt_version=prompt_version)
     return result
